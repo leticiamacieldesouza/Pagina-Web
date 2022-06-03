32f1ccc06fe2db8acd482354d4c667f1bc31af26
Leticia Maciel de Souza <100486813+leticiamacieldesouza@users.noreply.github.com>
2022-06-02 22:02:25 -0300

    Add files via upload

---
+++ biogás.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Accordion - Default functionality</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="accordion">
+  <h3>Parte I: Digestão Anaeróbia 1</h3>
+  <div>
+    <p>
+	A digestão anaeróbia (D.A.) é uma tecnologia de tratamento aplicada a uma
+	ampla variedade de substratos orgânicos. Estes substratos são, em sua maioria,
+	resíduos provenientes de processos produtivos tais como os dejetos da produção de
+	animais, efluentes líquidos e resíduos sólidos das agroindustriais e resíduos da
+	agricultura. Dependendo do contexto local e da legislação pertinente, podem ainda ser
+	aproveitadas as culturas energéticas, que são espécies vegetais cultivadas
+	especificamente para a geração de energia.
+    </p>
+  </div>
+  <h3>Etapa 1 (Hidrólise)</h3>
+  <div>
+    <p>
+	A hidrólise (hidro = água e lysis = quebra) é a primeira etapa da D.A. Durante
+	esta etapa, os componentes químicos mais complexos (polímeros) presentes nos
+	substratos são degradados em meio aquoso e convertidos em moléculas menores
+	(monômeros), que são então consumidos durante as etapas posteriores.
+    </p>
+  </div>
+  <h3>Etapa 2 (Acidogênese)</h3>
+  <div>
+    <p>
+    Os monômeros formados na etapa de hidrólise como os açúcares simples, os
+	aminoácidos e os ácidos graxos de cadeia curta encontra-se são solúveis em água e,
+	portanto, podem ser absorvidos por bactérias anaeróbias e facultativas por meio de
+	diversos processos fermentativos.
+	Ao final de etapa de acidogênese são gerados principalmente dióxido de
+	carbono (CO2), hidrogênio (H2), álcoois e ácidos orgânicos. Os compostos orgânicos
+	nitrogenados e compostos orgânicos sulfurados também são formados nesta etapa
+	devido à degradação de aminoácidos e proteínas.
+    </p>
+  </div>
+  <h3>Etapa 3 (Acetogênese</h3>
+  <div>
+    <p>
+    Nesta etapa, a maioria dos ácidos e álcoois, como o ácido butírico, ácido
+	propiônico e etanol, por exemplo, produzidos durante a acidogênese são degradados
+	em acetato, que pode ser utilizado como substrato para as arqueas metanogênicas.
+    </p>
+    <ul>
+      <li>List item one</li>
+      <li>List item two</li>
+      <li>List item three</li>
+    </ul>
+  </div>
+  <h3>Etapa 4 (Metanogênese)</h3>
+  <div>
+    <p>
+    A metanogênese é a última etapa do processo de degradação da matéria orgânica. 
+	Nela o metano é formado principalmente a partir de acetato, CO2 e H2 por bactérias 
+	estritamente anaeróbias..
+    </p>
+</div>
+ 
+ 
+</body>
+</html>
